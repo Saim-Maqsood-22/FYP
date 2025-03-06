0267081fda890e1dc7cfdac480db58a01884012d
Saim-Maqsood <97038010+Saim-Maqsood@users.noreply.github.com>
2025-03-06 22:27:53 +0500

    remove navigation from footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,24 +5,7 @@ const Footer = () => {
     <footer className="bg-#1a1a1a text-white py-6"> 
       <div className="container mx-auto text-center">
         <p>&copy; {new Date().getFullYear()} medMagic. All rights reserved.</p>
-        <div className="mt-4">
-          <Link href="/" className="text-white hover:text-gray-200 mx-2">
-            Home
-          </Link>
-          <Link href="/Maintainance" className="text-white hover:text-gray-200 mx-2">
-            Store
-          </Link>
-          <Link href="/about" className="text-white hover:text-gray-200 mx-2">
-            About
-          </Link>
-          <Link href="/Maintainance" className="text-white hover:text-gray-200 mx-2">
-            Contact
-          </Link>
-          <Link href="/FAQ" className="text-white hover:text-gray-200 mx-2">
-            FAQs
-          </Link>
         </div>
-      </div>
     </footer>
   );
 };
